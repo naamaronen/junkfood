cddd278d9eafb1f72f118bc66d7164292f72f5a1
Ornahm <59653355+Ornahm@users.noreply.github.com>
2020-02-24 17:12:20 +0200

    delete review updated

--- src/client/reducers/userReducer.js
+++ src/client/reducers/userReducer.js
@@ -1,7 +1,6 @@
 
 import {
   USER_PROFILE_SUCCSSES,
-  OTHER_PROFILE_SUCCSSES,
   LOADED_USERS,
   LOGOUT, ADD_REVIEW_SUCCESS
 } from "../actions/types";
@@ -21,8 +20,6 @@ export default function(state = initialState, action) {
       return { ...state, userProfile: action.payload, geoLocation: JSON.parse(action.payload.geolocation) };
     case LOGOUT:
       return {...state, userProfile: null, geoLocation: null }
-    case OTHER_PROFILE_SUCCSSES:
-      return { ...state, otherUser: action.payload };
     case ADD_REVIEW_SUCCESS:
       state.userProfile.reviews.push(action.payload.review);
       return {
